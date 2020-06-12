e53f46c4789f7753fd50ce5b584b4bc4e36201aa
余浩 <980187223@qq.com>
2020-06-12 14:58:44 +0800

    从request对象的map集合中存取数据(数组)

---
+++ Test/src/cn/tedu/Servlet/TestScope02.java
@@ -0,0 +1,27 @@
+package cn.tedu.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TestScope02", urlPatterns = "/TestScope02")
+public class TestScope02 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        //----------------------------------------------------------
+        System.out.println("TestScope02.doGet()...");
+        //从request对象的map集合中取出数据(数组)
+        String names = (String) request.getAttribute("names");
+        //做出响应
+        out.write("测试(转发+域对象)带数据到目的地");
+    }
+}
