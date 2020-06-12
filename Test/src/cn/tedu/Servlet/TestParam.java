2020c011100d73b35e0ccf798ab144d6cc973052
余浩 <980187223@qq.com>
2020-06-12 11:33:56 +0800

    11；33

---
+++ Test/src/cn/tedu/Servlet/TestParam.java
@@ -0,0 +1,32 @@
+package cn.tedu.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+@WebServlet(name = "TestParam")
+public class TestParam extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //处理POST提交中文乱码问题
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        //获取请求中的用户名(getParameter)
+        String user = request.getParameter("user");
+        //获取请求中的爱好(getParameterValues)
+        String[] like = request.getParameterValues("like");
+        System.out.println(
+                Arrays.toString(like)
+        );
+        out.write("测试request");
+    }
+}
---
+++ Test/src/cn/tedu/Servlet/TestParam2.java
@@ -0,0 +1,39 @@
+package cn.tedu.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(name = "TestParam2")
+public class TestParam2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.获取用户名(username)
+        String username = request.getParameter("username");
+        System.out.println("username"+username);
+        //2.获取密码(pwd)
+        String pwd = request.getParameter("pwd");
+        System.out.println("pwd"+pwd);
+        //3.获取性别(gender)
+        String gender = request.getParameter("gender");
+        System.out.println("gender"+gender);
+        //4.获取爱好(like)
+        String[] like = request.getParameterValues("like");
+        System.out.println(
+                Arrays.toString(like)
+        );
+        //5.获取城市(city)
+        String city = request.getParameter("city");
+        System.out.println("city"+city);
+        //6.获取自我描述(description)
+        String description = request.getParameter("description");
+        System.out.println("description"+description);
+    }
+}
